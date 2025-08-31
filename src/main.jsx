9f6175a061d675715fa9b8e218e21f8e20f73078
ennoblementcenter-hue <ennoblementcenter@gmail.com>
2025-08-31 15:35:24 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-console.log("MAIN.JSX EXECUTED");
+createRoot(document.getElementById("root")).render(<App />);
